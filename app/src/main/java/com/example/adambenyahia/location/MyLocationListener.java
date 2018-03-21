d9c92d82915ddbdcbbc9dfcb1bf96413e475b0d6
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-03-21 22:46:12 +0100

    google map added

---
+++ app/src/main/java/com/example/adambenyahia/location/MyLocationListener.java
@@ -0,0 +1,31 @@
+package com.example.adambenyahia.location;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by adambenyahia on 20/03/2018.
+ */
+
+public class MyLocationListener implements LocationListener {
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
